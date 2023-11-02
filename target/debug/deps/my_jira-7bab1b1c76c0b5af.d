514fe5db3a39401a2b46a40527b4afefe257bb8d
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-02 11:30:46 +0200

    init

---
+++ target/debug/deps/my_jira-7bab1b1c76c0b5af.d
@@ -0,0 +1,6 @@
+C:\Users\Ivan_Borovyk\Desktop\projects\rust\jira-rust\target\debug\deps\my_jira-7bab1b1c76c0b5af.rmeta: src\main.rs src\models.rs
+
+C:\Users\Ivan_Borovyk\Desktop\projects\rust\jira-rust\target\debug\deps\my_jira-7bab1b1c76c0b5af.d: src\main.rs src\models.rs
+
+src\main.rs:
+src\models.rs:
---
+++ target/debug/deps/my_jira-a3c1ff3cfdf90c74.d
@@ -0,0 +1,6 @@
+C:\Users\Ivan_Borovyk\Desktop\projects\rust\jira-rust\target\debug\deps\my_jira-a3c1ff3cfdf90c74.rmeta: src\main.rs src\models.rs
+
+C:\Users\Ivan_Borovyk\Desktop\projects\rust\jira-rust\target\debug\deps\my_jira-a3c1ff3cfdf90c74.d: src\main.rs src\models.rs
+
+src\main.rs:
+src\models.rs:
