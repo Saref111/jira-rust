a75f6ade3b4267100037a77e91dcae4bc74a76db
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-02 11:54:16 +0200

    update

--- target/debug/deps/my_jira-a3c1ff3cfdf90c74.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Ivan_Borovyk\Desktop\projects\rust\jira-rust\target\debug\deps\my_jira-a3c1ff3cfdf90c74.rmeta: src\main.rs src\models.rs
-
-C:\Users\Ivan_Borovyk\Desktop\projects\rust\jira-rust\target\debug\deps\my_jira-a3c1ff3cfdf90c74.d: src\main.rs src\models.rs
-
-src\main.rs:
-src\models.rs:
